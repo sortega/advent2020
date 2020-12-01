831d1f409b0fa395b11a8639c5798c491ee84892
Sebastián Ortega <sebastian.ortega@letgo.com>
2020-12-01 22:06:45 +0100

    Day 1

---
+++ src/test/scala/aoc/Day01Test.scala
@@ -0,0 +1,23 @@
+package aoc
+
+import org.junit.Test
+import org.junit.Assert._
+
+class Day01Test {
+  val input = List(
+    1721,
+    979,
+    366,
+    299,
+    675,
+    1456
+  )
+
+  @Test def part1Test(): Unit = {
+    assertEquals(514579, part1(input))
+  }
+
+  @Test def part2Test(): Unit = {
+    assertEquals(241861950, part2(input))
+  }
+}
