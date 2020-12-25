93de66ff0404e577e0870c08713cd43007a3a686
Sebastián Ortega <sebastian.ortega@letgo.com>
2020-12-25 10:21:41 +0100

    Day25 part 1

---
+++ src/test/scala/aoc/Day25Test.scala
@@ -0,0 +1,16 @@
+package aoc
+
+import aoc.Day25._
+import org.junit.Assert._
+import org.junit.Test
+
+class Day25Test {
+
+  @Test def part1Test(): Unit = {
+    assertEquals(14897079L, part1(cardPk = 5764801, doorPk = 17807724))
+  }
+
+//  @Test def part2Test(): Unit = {
+//  }
+
+}
